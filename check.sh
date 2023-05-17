109830664a625bbf4b080f7749804b2eda893dad
yuant <42244015+yuant2021@users.noreply.github.com>
2023-05-17 21:50:00 +0800

    Fix Openai

--- check.sh
+++ check.sh
@@ -3686,7 +3686,7 @@ function Sport_UnlockTest() {
 
 function Openai_UnlockTest() {
     echo "==============[ Openai ]==============="
-    local tmpresult=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -fsLI --output /dev/null --max-time 10 "https://chat.openai.com" 2>&1)
+    local tmpresult=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -SsLI --output /dev/null --max-time 10 "https://chat.openai.com" 2>&1)
     if [[ "$tmpresult" == "curl"* ]]; then
         echo -n -e "\r Openai:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
         return
@@ -3696,7 +3696,7 @@ function Openai_UnlockTest() {
     if [ ! -n "$result1" ]; then
     	echo -n -e "\r Openai:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
     else
-    	local region1=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -fsL --max-time 10 "https://chat.openai.com/cdn-cgi/trace" 2>&1 | grep "loc=" | awk -F= '{print $2}')
+    	local region1=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -SsL --max-time 10 "https://chat.openai.com/cdn-cgi/trace" 2>&1 | grep "loc=" | awk -F= '{print $2}')
         echo -n -e "\r Openai:\t\t\t\t${Font_Green}Yes (Region: ${region1})${Font_Suffix}\n"
     fi
 
