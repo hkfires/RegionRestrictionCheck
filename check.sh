93f564d208058f14f72dd1e40ddef248431ea776
yuant <42244015+yuant2021@users.noreply.github.com>
2024-03-28 22:09:10 +0800

    Update Netflix

--- check.sh
+++ check.sh
@@ -427,8 +427,8 @@ function MediaUnlockTest_BBCiPLAYER() {
 }
 
 function MediaUnlockTest_Netflix() {
-    local result1=$(curl $curlArgs -${1} -fsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/81280792" | grep -Eo \"isPlayable\":....  2>&1)
-    local result2=$(curl $curlArgs -${1} -fsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/70143836" | grep -Eo \"isPlayable\":.... 2>&1)
+    local result1=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -fsLI -X GET --write-out %{http_code} --output /dev/null --max-time 10 --tlsv1.3 "https://www.netflix.com/title/81280792"  2>&1)
+    local result2=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -fsLI -X GET --write-out %{http_code} --output /dev/null --max-time 10 --tlsv1.3 "https://www.netflix.com/title/70143836" 2>&1)
     local regiontmp=$(curl $curlArgs -${1} -fSs --max-time 10 --write-out %{redirect_url} --output /dev/null --tlsv1.3 "https://www.netflix.com/login" 2>&1 )
     if [[ "$regiontmp" == "curl"* ]]; then
         echo -n -e "\r Netflix:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
@@ -438,11 +438,17 @@ function MediaUnlockTest_Netflix() {
     if [[ ! -n "$region" ]]; then
         region="US"
 	fi
-    if [[ "$result1" == *"true" ]] || [[ "$result2" == *"true" ]]; then
+    if [[ "$result1" == "404" ]] && [[ "$result2" == "404" ]]; then
+        echo -n -e "\r Netflix:\t\t\t\t${Font_Yellow}Originals Only (Region: ${region})${Font_Suffix}\n"
+        return
+    elif [[ "$result1" == "403" ]] && [[ "$result2" == "403" ]]; then
+        echo -n -e "\r Netflix:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
+        return
+    elif [[ "$result1" == "200" ]] || [[ "$result2" == "200" ]]; then
         echo -n -e "\r Netflix:\t\t\t\t${Font_Green}Yes (Region: ${region})${Font_Suffix}\n"
         return
-    else
-        echo -n -e "\r Netflix:\t\t\t\t${Font_Yellow}Originals Only (Region: ${region})${Font_Suffix}\n"
+    elif [[ "$result1" == "000" ]]; then
+        echo -n -e "\r Netflix:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
         return
     fi
     echo -n -e "\r Netflix:\t\t\t\t${Font_Red}Failed${Font_Suffix}\n"
