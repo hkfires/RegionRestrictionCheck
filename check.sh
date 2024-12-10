14a17a67ddd2f1c45b11aee31e0e96f0206a52c0
yuant <42244015+yuant2021@users.noreply.github.com>
2024-12-10 11:01:46 +0800

    Fix typo

--- check.sh
+++ check.sh
@@ -3730,15 +3730,15 @@ function MediaUnlockTest_ChatGPT() {
 function MediaUnlockTest_Sora() {
     local tmpresult=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -SsLI --max-time 10 "https://sora.com" 2>&1)
     if [[ "$tmpresult" == "curl"* ]]; then
-        echo -n -e "\r Sora:\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
+        echo -n -e "\r Sora:\t\t\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n"
         return
     fi
     local result1=$(echo "$tmpresult" | grep 'location' )
     if [ ! -n "$result1" ]; then
-    	echo -n -e "\r Sora:\t\t\t\t${Font_Red}No${Font_Suffix}\n"
+    	echo -n -e "\r Sora:\t\t\t\t\t${Font_Red}No${Font_Suffix}\n"
     else
     	local region1=$(curl $curlArgs -${1} --user-agent "${UA_Browser}" -SsL --max-time 10 "https://sora.com/cdn-cgi/trace" 2>&1 | grep "loc=" | awk -F= '{print $2}')
-        echo -n -e "\r Sora:\t\t\t\t${Font_Green}Yes (Region: ${region1})${Font_Suffix}\n"
+        echo -n -e "\r Sora:\t\t\t\t\t${Font_Green}Yes (Region: ${region1})${Font_Suffix}\n"
     fi
 }
 
