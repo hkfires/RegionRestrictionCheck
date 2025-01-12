75de5b83a5429e0d5c4a2afbd27c9d8c13a5a007
yuant <42244015+yuant2021@users.noreply.github.com>
2025-01-12 16:39:18 +0800

    Fix bug

--- check.sh
+++ check.sh
@@ -219,8 +219,8 @@ if [ -z "$func" ]; then
     wait
     # bgptools_v4=$(curl $curlArgs -s -4 --max-time 10 --user-agent "${UA_Browser}" "https://v4.bgp.tools/whoami-not-for-robots" &)
     # bgptools_v6=$(curl $curlArgs -s -6 --max-time 10 --user-agent "${UA_Browser}" "https://v6.bgp.tools/whoami-not-for-robots" &)
-    ripe_stat_v4=$(curl $curlArgs -s --max-time 10"https://stat.ripe.net/data/prefix-overview/data.json?resource=$local_ipv4" &)
-    ripe_stat_v6=$(curl $curlArgs -s --max-time 10"https://stat.ripe.net/data/prefix-overview/data.json?resource=$local_ipv6" &)
+    ripe_stat_v4=$(curl $curlArgs -s --max-time 10 "https://stat.ripe.net/data/prefix-overview/data.json?resource=$local_ipv4" &)
+    ripe_stat_v6=$(curl $curlArgs -s --max-time 10 "https://stat.ripe.net/data/prefix-overview/data.json?resource=$local_ipv6" &)
     wait
     local_isp4=$(echo $ripe_stat_v4 | jq .data.asns[0].holder | tr -d '"')
     local_as4=$(echo $ripe_stat_v4 | jq .data.asns[0].asn | tr -d '"')
