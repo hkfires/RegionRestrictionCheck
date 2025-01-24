df5f3ab0a93a829dc4ab165219a46c56f656a372
yuant <42244015+yuant2021@users.noreply.github.com>
2025-01-24 22:09:18 +0800

    Fix Bugs

--- check.sh
+++ check.sh
@@ -1154,14 +1154,14 @@ function MediaUnlockTest_YouTube_CDN() {
     if [[ "$cdn_node" == *"-"* ]]; then
         local CDN_ISP=$(echo $cdn_node | cut -f1 -d"-" | tr [:lower:] [:upper:])
         local CDN_LOC=$(echo $cdn_node | cut -f2 -d"-" | sed 's/[^a-z]//g')
-        local lineNo=$(echo "${CDN_LOC^^}" | cut -f3 -d"|" | sed -n "/${iata}/=")
+        local lineNo=$(echo "${CDN_LOC^^}" | cut -f3 -d"|" | sed -n "/${CDN_LOC}/=")
         local location=$(echo "${CDN_LOC^^}" | awk "NR==${lineNo}" | cut -f1 -d"|" | sed -e 's/^[[:space:]]*//')
         echo -n -e "\r YouTube CDN:\t\t\t\t${Font_Yellow}$CDN_ISP in $location ($cdn_node)${Font_Suffix}\n"
         return
     fi
     if [[ "$cdn_node" == *"."* ]]; then
         local CDN_LOC=$(echo $cdn_node | cut -f2 -d"-" | cut -c1-3)
-        local lineNo=$(echo "${CDN_LOC^^}" | cut -f3 -d"|" | sed -n "/${iata}/=")
+        local lineNo=$(echo "${CDN_LOC^^}" | cut -f3 -d"|" | sed -n "/${CDN_LOC}/=")
         local location=$(echo "${CDN_LOC^^}" | awk "NR==${lineNo}" | cut -f1 -d"|" | sed -e 's/^[[:space:]]*//')
         echo -n -e "\r YouTube CDN:\t\t\t\t${Font_Green}$location ($cdn_node)${Font_Suffix}\n"
         return
